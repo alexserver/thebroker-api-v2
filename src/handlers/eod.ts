132bd894e2d96d275ca0cd0913bbd92b2228023e
Alejandro Gomez <alexserverone@gmail.com>
2024-02-28 20:51:01 -0600

    adding try catch to handlers/eod

--- src/handlers/eod.ts
+++ src/handlers/eod.ts
@@ -3,22 +3,28 @@ import { getEodHistory, getTickerEod } from '../orm/eod'
 
 export const eodOne = async ({ params }: Context) => {
   const { symbol, when } = params
-  const data = await getTickerEod({ symbol, when })
-  if (data) {
-    return data
-  } else {
-    return {}
+  try {
+    const data = await getTickerEod({ symbol, when })
+    if (data) {
+      return data
+    } else {
+      return {}
+    }
+  } catch (err) {
+    throw new Error('Error fetching ticker EOD')
   }
-  return
 }
 
 export const eodAll = async ({ query }: Context) => {
   const { symbols, date_from, date_to } = query
-  const data = await getEodHistory({ symbols, date_from, date_to })
-  if (data) {
-    return data
-  } else {
-    return {}
+  try {
+    const data = await getEodHistory({ symbols, date_from, date_to })
+    if (data) {
+      return data
+    } else {
+      return {}
+    }
+  } catch (err) {
+    throw new Error('Error fetching EOD history')
   }
-  return
 }
