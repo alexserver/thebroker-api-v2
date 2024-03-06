137744ab7f1398e78ea04cf25ee62591f448584b
Alejandro Gomez <alexserverone@gmail.com>
2024-03-06 17:17:47 -0600

    moving authorization from headers to query params ?access_key , this is to mimic the original marketstack API auth mode.

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -2,9 +2,14 @@ import { Elysia } from 'elysia'
 
 const API_KEY = process.env.API_KEY
 
+const getParamFromURL = (url: string, param: string) => {
+  const { searchParams } = new URL(url)
+  return searchParams.get(param) ?? ''
+}
+
 export const auth = new Elysia() //
-  .onRequest(({ set, request: { headers } }) => {
-    const access_key = headers.get('access-key') ?? ''
+  .onRequest(({ set, request: { url } }) => {
+    const access_key = getParamFromURL(url, 'access_key')
     if (access_key !== API_KEY) {
       return (set.status = 401) // Unauthorized
     }
